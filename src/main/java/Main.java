7a3e0bd7849e06c62bb61a429ba621ec02120fe2
Sebastian Ciulacu <sebastian.ciulacu@gmail.com>
2019-11-26 19:37:17 +0200

    Added pom and a main Class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+    Persoana Ion = new Persoana("Ion");
+    Ion.setAge(9);
+        System.out.println(Ion.getAgePers());
     }
 }
---
+++ src/main/java/Persoana.java
@@ -0,0 +1,12 @@
+public class Persoana {
+    int agePers;
+    public Persoana(String name){
+
+    }
+    public void setAge(int age){
+        agePers = age;
+    }
+    public int getAgePers(){
+        return agePers;
+    }
+}
